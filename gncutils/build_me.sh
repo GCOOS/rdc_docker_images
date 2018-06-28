090b492b2383fde296e768c8660f0a648ac68384
Bob Currier <robertdcurrier@gmail.com>
2018-06-28 10:53:31 -0400

    added seawater and geojson to gncutils; removed git pull as JK working on source

--- gncutils/build_me.sh
+++ gncutils/build_me.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Clone latest repo
-echo "Cloning gncutils..."
-if [ ! -d src/gncutils ]; then
-  git clone https://github.com/kerfoot/gncutils.git src/gncutils
-else
-  cd src/gncutils
-  git pull
-fi
+#echo "Cloning gncutils..."
+#if [ ! -d src/gncutils ]; then
+#  git clone https://github.com/kerfoot/gncutils.git src/gncutils
+#else
+#  cd src/gncutils
+#  git pull
+#fi
 
 # Back to top level
-cd ~/src/docker/docker_images/gncutils
+#cd ~/src/docker/docker_images/gncutils
 
 # Build image
 docker build -t robertdcurrier/gncutils .
--- gncutils/gandalf_build_nc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /opt/gncutils/scripts
-./dba_to_ngdac_profile_nc.py -p 1 \
-/data/gandalf/gandalf_configs/$2 \
-/data/gandalf/deployments/$1/$2/processed_data/$2.merged.dba \
--o /data/gandalf/deployments/$1/$2/ngdac_files
