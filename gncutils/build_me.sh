405a46c373c762333caa82e17111c2fbba8f7374
Bob Currier <robertdcurrier@gmail.com>
2019-09-27 19:06:23 +0000

    moved gncutils to ubuntu

--- gncutils/build_me.sh
+++ gncutils/build_me.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-# Clone latest repo
-#echo "Cloning gncutils..."
-#if [ ! -d src/gncutils ]; then
-#  git clone https://github.com/kerfoot/gncutils.git src/gncutils
-#else
-#  cd src/gncutils
-#  git pull
-#fi
-
-# Back to top level
-#cd ~/src/docker/docker_images/gncutils
-
 # Build image
-docker build -t robertdcurrier/gncutils .
-docker push robertdcurrier/gncutils
+docker build -t robertdcurrier/ubuntu_gncutils .
+docker push robertdcurrier/ubuntu_gncutils
 
