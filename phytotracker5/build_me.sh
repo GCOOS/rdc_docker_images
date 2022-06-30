add0c357940ae08865212d652b6ecd3359628b1c
Bob Currier <robertdcurrier@gmail.com>
2022-06-30 15:15:55 -0400

    updated ubuntu level in u_a_p3, and mongo in habforecast

--- phytotracker5/build_me.sh
+++ phytotracker5/build_me.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Build image
-docker build -t robertdcurrier/phytotracker5 .
-#docker push robertdcurrier/phytotracker5
+#docker build -t robertdcurrier/phytotracker5 .
+docker push robertdcurrier/phytotracker5
 
