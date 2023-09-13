05f708b0d2d64619c38d9c89e75f53d79f8d956e
Bob Currier <robertdcurrier@gmail.com>
2023-09-13 13:14:54 -0400

    restored full gncutils source

--- gncutils/build_me.sh
+++ gncutils/build_me.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Build image
-docker build -t robertdcurrier/ubuntu_gncutils_2021 .
-docker push robertdcurrier/ubuntu_gncutils_2021
+docker build -t robertdcurrier/ubuntu_gncutils_2023 .
+docker push robertdcurrier/ubuntu_gncutils_2023
 
